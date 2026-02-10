e73dec6385ba6279a02b0d218c8f03a2a2e84eef
Daffa Althaf <daffaalthaf6@gmail.com>
2026-02-10 22:13:25 +0700

    week01: (tugas 2) implement receipt printing

--- src/ oop_76981_DaffaAlthafmaulana/week01/GameStore.kt
+++ src/ oop_76981_DaffaAlthafmaulana/week01/GameStore.kt
@@ -3,11 +3,22 @@ package ` oop_76981_DaffaAlthafmaulana`.week01
 fun main() {
     val gameTitle = "Cyberpunk 2077"
     val price = 650000
-}
+    val discount = calculateDiscount(price)
+    val finalPrice = price - discount
 
+    printReceipt(
+        title = gameTitle,
+        finalPrice = finalPrice
+    )
+}
 fun calculateDiscount(price: Int): Int =
     if (price > 500000) {
         price * 20 / 100
     } else {
         price * 10 / 100
     }
+
+fun printReceipt(title: String, finalPrice: Int) {
+    println("Judul Game   : $title")
+    println("Harga Akhir  : Rp $finalPrice")
+}
