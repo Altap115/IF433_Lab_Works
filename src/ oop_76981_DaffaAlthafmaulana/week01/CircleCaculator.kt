c6d377f1cdfd382e70ca9ef44be5fada143f713e
Daffa Althaf <daffaalthaf6@gmail.com>
2026-02-10 22:05:40 +0700

    week01: (tugas 1) refactor variables to val and use string templates

--- src/ oop_76981_DaffaAlthafmaulana/week01/CircleCaculator.kt
+++ src/ oop_76981_DaffaAlthafmaulana/week01/CircleCaculator.kt
@@ -2,11 +2,11 @@ package ` oop_76981_DaffaAlthafmaulana`.week01
 
 fun main(args: Array<String>) {
     // Variable definition
-    var radius: Double = 7.0
-    var pi: Double = 3.14
+    val radius: Double = 7.0
+    val pi: Double = 3.14
     // Calculation
-    var area: Double = pi * radius * radius
-    println("Radius: " + radius + ", Area: " + area)
+    val area = pi * radius * radius
+    println("Radius:  $radius , Area: $area")
     // Logic check
     checkSize(area)
 }
