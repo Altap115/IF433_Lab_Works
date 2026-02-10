e0fd5c57dd2509cb9c313aad4e660886d2302cf5
Daffa Althaf <daffaalthaf6@gmail.com>
2026-02-10 21:44:27 +0700

    week01 : initial setup and procedural code

---
+++ src/ oop_76981_DaffaAlthafmaulana/week01/GradingSystem.kt
@@ -0,0 +1,8 @@
+package ` oop_76981_DaffaAlthafmaulana`.week01
+
+fun main() {
+    var name  : String ="John Thor"
+    val score : Int =  80
+
+    println("nama: " + name + ", Nilai: " + score)
+}
