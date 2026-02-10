b84f7972a50cbc2eadf79e8b89731120770cc4ce
Daffa Althaf <daffaalthaf6@gmail.com>
2026-02-10 22:12:50 +0700

    week01: (tugas 2) implement discount logic

--- src/ oop_76981_DaffaAlthafmaulana/week01/GameStore.kt
+++ src/ oop_76981_DaffaAlthafmaulana/week01/GameStore.kt
@@ -4,3 +4,10 @@ fun main() {
     val gameTitle = "Cyberpunk 2077"
     val price = 650000
 }
+
+fun calculateDiscount(price: Int): Int =
+    if (price > 500000) {
+        price * 20 / 100
+    } else {
+        price * 10 / 100
+    }
