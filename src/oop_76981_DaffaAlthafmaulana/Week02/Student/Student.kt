3fce73cbdef42d150b45bb5e06ceb8a2f9a8ff0f
Daffa Althaf <daffaalthaf6@gmail.com>
2026-02-25 03:01:27 +0700

    week02: add warning validation in init block

--- src/oop_76981_DaffaAlthafmaulana/Week02/Student/Student.kt
+++ src/oop_76981_DaffaAlthafmaulana/Week02/Student/Student.kt
@@ -1,9 +1,12 @@
 package oop_76981_DaffaAlthafmaulana.Week02.Student
 
-class Student (
-    val name: String,
-    val nim:String,
-    var major: String
-){
-
+class Student (val name: String, val nim:String, var major: String){
+    init {
+        if (nim.length != 5){
+        println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
+        println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
+        } else {
+        println("LOG: Objek Student $name berhasil dialokasi di Memory.")
+        }
+    }
 }
