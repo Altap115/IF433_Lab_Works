bedfe28757d5f7570fffe5f1e068b29672335d3c
Daffa Althaf <daffaalthaf6@gmail.com>
2026-02-25 02:56:58 +0700

    week02: create basic Student class structure

--- src/oop_76981_DaffaAlthafmaulana/Week02/Student/Student.java
+++
@@ -1,8 +0,0 @@
-package oop_76981_DaffaAlthafmaulana.Week02.Student;
-
-public class Student (
-        val name : String,
-        val nim: String,
-        var major: String
-){
-}
---
+++ src/oop_76981_DaffaAlthafmaulana/Week02/Student/Student.kt
@@ -0,0 +1,9 @@
+package oop_76981_DaffaAlthafmaulana.Week02.Student
+
+class Student (
+    val name: String,
+    val nim:String,
+    var major: String
+){
+
+}
