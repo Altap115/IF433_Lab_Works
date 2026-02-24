6c10f807ac39a0d76d96b2a3e8639c06ff94e78e
Daffa Althaf <daffaalthaf6@gmail.com>
2026-02-25 04:22:00 +0700

    week03: cleanup code

--- src/oop_76981_DaffaAlthafmaulana/Week03/main.kt
+++ src/oop_76981_DaffaAlthafmaulana/Week03/main.kt
@@ -3,15 +3,12 @@ package oop_76981_DaffaAlthafmaulana.Week03
 fun main() {
     val e = Employee("Budi")
 
-    // 1. Test Validasi Salary
-    e.salary = -1000   // Harusnya print error
+
+    e.salary = -1000
     e.salary = 5_000_000
     println("Gaji: ${e.salary}")
 
-    // 2. Test Encapsulation
     e.increasePerformance()
-    // e.performanceRating = 5  // Kalau di-uncomment pasti error
 
-    // 3. Test Computed Property
     println("Pajak yang harus dibayar: ${e.tax}")
 }
