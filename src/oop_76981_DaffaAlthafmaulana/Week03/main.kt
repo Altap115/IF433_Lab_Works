2da09603cb308086504277baead06397fb5bb3ca
Daffa Althaf <daffaalthaf6@gmail.com>
2026-02-25 04:21:02 +0700

    week03: test all encapsulation logic in main

--- src/oop_76981_DaffaAlthafmaulana/Week03/main.kt
+++ src/oop_76981_DaffaAlthafmaulana/Week03/main.kt
@@ -2,6 +2,16 @@ package oop_76981_DaffaAlthafmaulana.Week03
 
 fun main() {
     val e = Employee("Budi")
-    println("--- Test Error ---")
-    e.salary = 5000
+
+    // 1. Test Validasi Salary
+    e.salary = -1000   // Harusnya print error
+    e.salary = 5_000_000
+    println("Gaji: ${e.salary}")
+
+    // 2. Test Encapsulation
+    e.increasePerformance()
+    // e.performanceRating = 5  // Kalau di-uncomment pasti error
+
+    // 3. Test Computed Property
+    println("Pajak yang harus dibayar: ${e.tax}")
 }
