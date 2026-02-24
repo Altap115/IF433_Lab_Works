af5533315eaa1092c50d09cf4fb31b314b3e2e81
Daffa Althaf <daffaalthaf6@gmail.com>
2026-02-25 04:46:34 +0700

    week04: (task 1) test ElectricCar in main

---
+++ src/oop_76981_DaffaAlthafmaulana/Week04/main2.kt
@@ -0,0 +1,11 @@
+package oop_76981_DaffaAlthafmaulana.Week04
+
+fun main2() {
+    println("----- ELECTRIC CAR TEST -----")
+
+    val tesla = ElectricCar("Tesla", 4, 90)
+
+    tesla.accelerate()
+    tesla.honk()
+    tesla.openTrunk()
+}
