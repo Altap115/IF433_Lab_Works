26f722d69d0a72446af7614df2e496ab6195295a
Daffa Althaf <daffaalthaf6@gmail.com>
2026-02-25 04:48:31 +0700

    week04: (task 2) create Manager class overriding Employee

---
+++ src/oop_76981_DaffaAlthafmaulana/Week04/Manager.kt
@@ -0,0 +1,15 @@
+package oop_76981_DaffaAlthafmaulana.Week04
+
+class Manager(
+    name: String,
+    baseSalary: Int
+) : Employee(name, baseSalary) {
+
+    override fun work() {
+        println("$name sedang memimpin rapat divisi.")
+    }
+
+    override fun calculateBonus(): Int {
+        return super.calculateBonus() + 500_000
+    }
+}
