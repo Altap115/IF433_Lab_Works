8eb4585149f611f171b120821441dd35adc264ce
Daffa Althaf <daffaalthaf6@gmail.com>
2026-03-10 21:49:34 +0700

    create Dosen class inheriting Pegawai

--- src/oop_76981_DaffaAlthafmaulana/Week05/Pegawai.kt
+++ src/oop_76981_DaffaAlthafmaulana/Week05/Pegawai.kt
@@ -3,3 +3,13 @@ package oop_76981_DaffaAlthafmaulana.Week05
 abstract class Pegawai (val nama: String) {
     abstract fun bekerja()
 }
+
+class Dosen(nama: String, val nidn: String): Pegawai(nama) {
+    override fun bekerja() {
+        println("[$nama] sedang menyiapkan materi perkuliahan dan merevisi RPKPS")
+    }
+
+    fun mengajar() {
+        println("[$nama] sedang mengajar di kelas")
+    }
+}
