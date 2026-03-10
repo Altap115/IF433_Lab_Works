2b3dd40e9fafccc73e81ce7abe6285e86ce024e4
Daffa Althaf <daffaalthaf6@gmail.com>
2026-03-10 22:13:49 +0700

    week05: (task 1) test MathHelper overloading in main

--- src/oop_76981_DaffaAlthafmaulana/Week05/Main.kt
+++ src/oop_76981_DaffaAlthafmaulana/Week05/Main.kt
@@ -23,3 +23,18 @@ fun Main() {
         println("-----------------------")
     }
 }
+
+fun main() {
+
+    println("===== TEST MATH HELPER =====")
+
+    val mathHelper = MathHelper()
+
+    val luasPersegi = mathHelper.hitungLuas(4)
+    val luasPersegiPanjang = mathHelper.hitungLuas(5, 3)
+    val luasLingkaran = mathHelper.hitungLuas(7.0)
+
+    println("Luas Persegi: $luasPersegi")
+    println("Luas Persegi Panjang: $luasPersegiPanjang")
+    println("Luas Lingkaran: $luasLingkaran")
+}
