7b94a13376fc40b65ce9c5be208e800ef6d4e496
Daffa Althaf <daffaalthaf6@gmail.com>
2026-03-10 22:05:04 +0700

    implement polymorphic collection and smart casting

---
+++ src/oop_76981_DaffaAlthafmaulana/Week05/Main.kt
@@ -0,0 +1,25 @@
+package oop_76981_DaffaAlthafmaulana.Week05
+
+fun Main() {
+    val dosen1 = Dosen ("Pak Alex", "0123456")
+    val admin1 = Admin("Bu Siti")
+
+    val daftarPegawai: List<Pegawai> = listOf(dosen1, admin1)
+
+    println("=== AKTIVITAS PEGAWAI ===")
+    for (pegawai in daftarPegawai) {
+        pegawai.bekerja()
+
+        when (pegawai){
+            is Dosen -> {
+                println("=> Terdeteksi sebagai Dosen (NIDN: ${pegawai.nidn})")
+                pegawai.mengajar()
+            }
+            is Admin -> {
+                println("=> Terdeteksi sebagai Admin")
+                pegawai.doAdminWork()
+            }
+        }
+        println("-----------------------")
+    }
+}
