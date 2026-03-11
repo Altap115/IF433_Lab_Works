701ac1c92fcd776d0786ab8c5d05772d4e053dcb
Daffa Althaf <daffaalthaf6@gmail.com>
2026-03-11 15:40:41 +0700

    week06: (task) create SmartHomeHub logic

---
+++ src/oop_76981_DaffaAlthafmaulana/Week06/SmartHomeHub.kt
@@ -0,0 +1,18 @@
+package oop_76981_DaffaAlthafmaulana.Week06
+
+class SmartHomeHub {
+
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice) {
+        devices.add(device)
+    }
+
+    fun turnOffAllSwitches() {
+        for (device in devices) {
+            if (device is Switchable) {
+                device.turnOff()
+            }
+        }
+    }
+
