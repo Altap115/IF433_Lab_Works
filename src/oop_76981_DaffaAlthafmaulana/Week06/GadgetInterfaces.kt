5ae431a9b1bd13e1a912633c0f75d7f370273048
Daffa Althaf <daffaalthaf6@gmail.com>
2026-03-11 15:19:48 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_76981_DaffaAlthafmaulana/Week06/GadgetInterfaces.kt
@@ -0,0 +1,9 @@
+package oop_76981_DaffaAlthafmaulana.Week06
+
+interface Camera {
+    fun turnOn() { println("Lensa kamera terbuka dan sensor aktif") }
+}
+
+interface Phone {
+    fun turnOn() { println("Sinyal seluler mencari jaringan.")}
+}
