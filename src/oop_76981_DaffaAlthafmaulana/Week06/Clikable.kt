10e6b2b90da799c167911435288e2f76dcac1f5f
Daffa Althaf <daffaalthaf6@gmail.com>
2026-03-11 15:03:08 +0700

    week06: fix Clickable interface using abstract property

--- src/oop_76981_DaffaAlthafmaulana/Week06/Clikable.kt
+++ src/oop_76981_DaffaAlthafmaulana/Week06/Clikable.kt
@@ -1,7 +1,6 @@
 package oop_76981_DaffaAlthafmaulana.Week06
 
 interface Clikable {
-    val name: String = "Tombol Rahasia"
-
+    val name: String // Abstract property, tidak ada nilai default
     fun click()
 }
