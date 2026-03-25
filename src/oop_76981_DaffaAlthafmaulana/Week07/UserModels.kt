79715e9dfa30d0516f8716c76b20c67ae6375803
Daffa Althaf <daffaalthaf6@gmail.com>
2026-03-25 15:13:30 +0700

    week07: implement data class for structural equality

--- src/oop_76981_DaffaAlthafmaulana/Week07/UserModels.kt
+++ src/oop_76981_DaffaAlthafmaulana/Week07/UserModels.kt
@@ -1,4 +1,4 @@
 package oop_76981_DaffaAlthafmaulana.Week07
 
-class RegularUser(val name: String, val age: Int) {
-}
+class RegularUser(val name: String, val age: Int) {}
+data class DataUser(var name: String, var age: Int)
