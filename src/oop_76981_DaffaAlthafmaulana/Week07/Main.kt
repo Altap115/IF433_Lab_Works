9a36d8eeda565b880506e48371bb3be7a6c3c13f
Daffa Althaf <daffaalthaf6@gmail.com>
2026-03-25 15:22:12 +0700

    week07: (trial) trigger non-exhaustive when compiler error

--- src/oop_76981_DaffaAlthafmaulana/Week07/Main.kt
+++ src/oop_76981_DaffaAlthafmaulana/Week07/Main.kt
@@ -25,4 +25,13 @@ fun Main() {
 
     val (userName, userAge) = data1
     println("Destructured: $userName berumur $userAge")
+
+
+    println("\n=== TEST SEALED CLASS ===")
+    val response: ApiResponse = ApiResponse.Success("Data berhasil ditarik!")
+
+    val uiMessage = when(response) {
+        is ApiResponse.Success -> "Tampilkan: ${response.data}"
+        is ApiResponse.Error -> "Munculkan alert: ${response.message}"
+    }
 }
