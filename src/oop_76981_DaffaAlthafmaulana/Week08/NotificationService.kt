69bf404f03d6ffa8d0a2dc47f8706124f51646f4
Daffa Althaf <daffaalthaf6@gmail.com>
2026-04-15 01:24:30 +0700

    week08: implement smart cast in function

---
+++ src/oop_76981_DaffaAlthafmaulana/Week08/NotificationService.kt
@@ -0,0 +1,21 @@
+package oop_76981_DaffaAlthafmaulana.Week08
+
+class NotificationService {
+    fun sendEmail(emailAddress: String) {
+        println("Mengirim email ke: $emailAddress")
+    }
+
+    fun processUser(user: UserProfile){
+        // sendEmail(user.email) // INI AKAN ERROR: Type mismatch
+
+        if (user.email != null) {
+            // SUCCESS via Smart Cast: compiler tahu 'user.email' pasti tidak null di blok ini
+
+            sendEmail(user.email)
+        } else {
+                println("User ${user.name} tidak memiliki email.")
+            }
+        }
+    }
+
+}
