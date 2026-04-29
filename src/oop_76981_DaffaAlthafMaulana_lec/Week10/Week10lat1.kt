44c6df66e8ed65cc77330440cb48f273d1a0ae45
Daffa Althaf <daffaalthaf6@gmail.com>
2026-04-29 13:51:44 +0700

    week10: test generic functions with Double and String

--- src/oop_76981_DaffaAlthafMaulana_lec/Week10/Week10lat1.kt
+++ src/oop_76981_DaffaAlthafMaulana_lec/Week10/Week10lat1.kt
@@ -7,8 +7,23 @@ class rumah(var alamat: Any)
 //code Generic
 class Kampus <T>(val alamat:T)
 
+//code Generic - Multi Parameter
+class Komputer<T, V>(val merk:T, val barcode:V)
+
+//code Generic - Function
+fun <T> Password(isiPass:T):T {
+    return isiPass
+}
 
 fun main() {
+    println("===== SAMPLE GENERIC - FFUNCTION =====")
+    println("Password Kamu: " + Password("12345"))
+
+    println("===== SAMPLE GENERIC - Multi Parameter =====")
+    val komp = Komputer("Dell", 12345)
+    println("Merk Komputer : " + komp.merk)
+    println("Barcode Komputer : ${komp.barcode}")
+
     println("===== SAMPLE NON-GENERIC =====")
     val rmh = rumah(alamat = 200)
     val angkaRumah = rmh.alamat as Int
--- src/oop_76981_DaffaAlthafmaulana/Week10/GenericMethod.kt
+++ src/oop_76981_DaffaAlthafmaulana/Week10/GenericMethod.kt
@@ -1,5 +1,9 @@
 package oop_76981_DaffaAlthafmaulana.Week10
 
+fun <T> printData(data: T){
+    println ("Data yang diterima: $data")
+}
+
 fun <T> processData(input: T): T {
     return input
 }
