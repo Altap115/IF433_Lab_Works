8f83f4c22e12976f9306f45ee97ed23739383c6c
Daffa Althaf <daffaalthaf6@gmail.com>
2026-04-29 14:08:33 +0700

    week10: (task) implement generic WalletRepository

---
+++ src/oop_76981_DaffaAlthafmaulana/Week10/WalletRepository.kt
@@ -0,0 +1,14 @@
+package oop_76981_DaffaAlthafmaulana.Week10
+
+class WalletRepository<T> {
+
+    private val items = mutableListOf<T>()
+
+    fun add(item: T) {
+        items.add(item)
+    }
+
+    fun getAll(): List<T> {
+        return items
+    }
+}
