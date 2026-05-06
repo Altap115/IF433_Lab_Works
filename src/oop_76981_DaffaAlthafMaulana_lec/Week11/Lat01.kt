eb4d57aa8dc1532cde35794fdae4893819377ab6
Daffa Althaf <daffaalthaf6@gmail.com>
2026-05-06 13:44:29 +0700

    week11: create string extension functions

---
+++ src/oop_76981_DaffaAlthafMaulana_lec/Week11/Lat01.kt
@@ -0,0 +1,50 @@
+package oop_76981_DaffaAlthafMaulana_lec.Week11
+
+fun Panggil_Nama(nama: String):String {
+    return "Hai $nama"
+}
+
+// Function Extension - BASIC
+fun String.Panggil_Mahasiswa():String {
+    //return "Hello $this"
+    var hasil = this.split(" ").joinToString(" ") {
+        it.replaceFirstChar {
+            c -> c.uppercase()
+        }
+    }
+    return hasil
+}
+
+fun Int.tentukanlulus(nama: String): String{
+    var keputusan = ""
+    if(this >= 70) {
+        keputusan = "LULUS"
+    } else {
+        keputusan = "REMEDIAL"
+    }
+    return "Mahasiswa $nama hasil kelulusan adalah $keputusan"
+}
+
+//Function Extension - cek Null
+fun String?.cekNulldanEmpty():String {
+    if(this == null || this.isEmpty()) {
+        return "gak boleh null atau kosong bang"
+    } else {
+        return "Username kamu adalah $this"
+    }
+}
+
+fun main () {
+    //cara panggil function extension - Null
+    var inputusername: String? = null
+    println(inputusername.cekNulldanEmpty())
+
+    //cara panggil function Extension - Parameter
+    println(80.tentukanlulus("Revan"))
+
+    // cara panggil function biasa
+    println(Panggil_Nama("Toni").uppercase())
+
+    //cara panggil function Extension - BASIC
+    println("tonnyyy stark ironman".Panggil_Mahasiswa())
+}
---
+++ src/oop_76981_DaffaAlthafmaulana/Week11/StringExtensions.kt
@@ -0,0 +1,11 @@
+package oop_76981_DaffaAlthafmaulana.Week11
+
+// Extension function dengan receiver 'this'
+fun String.addGreeting(): String {
+    return "Hello, $this"
+}
+
+// Extension function dengan parameter
+fun String.repeatTimes(n: Int): String {
+    return this.repeat(n)
+}
