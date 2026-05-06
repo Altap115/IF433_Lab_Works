1e2a4c566b90dcfb60de0379212fbcf41fe7205b
Daffa Althaf <daffaalthaf6@gmail.com>
2026-05-06 15:13:30 +0700

    week11: (task) configure Ezviz camera with also

--- src/oop_76981_DaffaAlthafmaulana/Week11/SmartHomeDashboard.kt
+++ src/oop_76981_DaffaAlthafmaulana/Week11/SmartHomeDashboard.kt
@@ -17,4 +17,11 @@ fun main() {
         homeDevices.add(it)
     }
 
+    SmartDevice("Ezviz Outdoor", "Camera").apply {
+        isOnline = true
+        powerLoad = 5
+    }.also {
+        println("(LOG) Kamera terhubung")
+        homeDevices.add(it)
+    }
 }
