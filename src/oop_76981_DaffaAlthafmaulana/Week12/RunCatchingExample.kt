56ba8aa1f9f415a74167262274887fc254191612
Daffa Althaf <daffaalthaf6@gmail.com>
2026-05-13 13:01:59 +0700

    week12: implement onSuccess and onFailure chaining

--- src/oop_76981_DaffaAlthafmaulana/Week12/RunCatchingExample.kt
+++ src/oop_76981_DaffaAlthafmaulana/Week12/RunCatchingExample.kt
@@ -9,4 +9,14 @@ fun main() {
     val safeValue = result.getOrElse { -1 }
     println("Safe Value (getOrElse): $safeValue")
 
+    val recovered = result.recover { 0 }.getOrNull()
+    println("Recovered Value: $recovered")
+
+    runCatching {
+        "100".toInt()
+    }.onSuccess { v ->
+        println("Berhasil dikonversi: $v")
+    }.onFailure { e ->
+        println("Gagal konversi: ${e.message}")
+    }
 }
