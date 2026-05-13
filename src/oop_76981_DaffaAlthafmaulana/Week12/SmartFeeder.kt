f3b71c5b91b6a5418c7374d4c9f034d49540c21b
Daffa Althaf <daffaalthaf6@gmail.com>
2026-05-13 13:17:26 +0700

    week12: (task) implement food stock validation with throw

--- src/oop_76981_DaffaAlthafmaulana/Week12/SmartFeeder.kt
+++ src/oop_76981_DaffaAlthafmaulana/Week12/SmartFeeder.kt
@@ -10,3 +10,7 @@ fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): I
     if (requestedGram > availableGram) {
         throw FoodEmptyException(requestedGram, availableGram)
     }
+
+    println("Kibble berhasil dikeluarkan!")
+    return availableGram - requestedGram
+}
