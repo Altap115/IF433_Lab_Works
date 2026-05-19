889058066ed3726c3872040ad5b79c0e54ef9941
Daffa Althaf <daffaalthaf6@gmail.com>
2026-05-20 01:43:00 +0700

    week13: implement printWriter with safe use block

--- src/oop_76981_DaffaAlthafmaulana/Week13/ResourceHandling.kt
+++ src/oop_76981_DaffaAlthafmaulana/Week13/ResourceHandling.kt
@@ -1,6 +1,7 @@
 package oop_76981_DaffaAlthafmaulana.Week13
 
 import java.io.File
+import java.io.PrintWriter
 
 fun main() {
     println("=== TEST UNSAFE RESOURCE HANDLING ===")
@@ -18,4 +19,16 @@ fun main() {
 // Wajib dipanggil secara manual jika tidak memakai blok 'use'
     writer.close()
     println("Proses penulisan unsafe selesai.")
+
+    println("\n=== TEST SAFE RESOURCE HANDLING ===")
+    val safeFile = File("safe_logs.txt")
+
+    // Writer akan OTOMATIS di-close saat keluar dari blok kurawal pembungkusnya
+    safeFile.printWriter().use { out ->
+        for (i in 1..100) {
+            out.println("Safe Log entry #$i: System status OK.")
+        }
+    }
+    println("100 baris log berhasil di-generate dengan sangat aman.")
+
 }
